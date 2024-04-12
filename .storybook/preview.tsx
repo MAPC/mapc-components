4c2902654719208b60fa0619b520c076255f7e93
NotLiu <aliu@lediur.com>
2024-04-12 17:42:55 -0500

    customize storybook theming, put in all mdx and stories for various categories and planned contents

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,23 +1,29 @@
 import React from "react";
 
-import type {Preview} from "@storybook/react";
+import type { Preview } from "@storybook/react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const preview: Preview = {
   parameters: {
-    actions: {argTypesRegex: "^on[A-Z].*"},
+    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
+    options: {
+      // storySort: (a, b) => {
+      //   const order = ["Overview", "Media", "Components", "Misc"];
+      //   return order.indexOf(a[1].kind) - order.indexOf(b[1].kind);
+      // },
+    },
     decorators: [
       (Story) => (
         <div
           className="test"
-          style={{margin: "2em"}}
+          style={{ margin: "2em" }}
         >
           <Story />
         </div>
