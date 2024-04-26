c86bf5e0516f275d5b02fec33ba91d8c9f1ad037
NotLiu <aliu@lediur.com>
2024-04-26 15:23:17 -0500

    resolve PR requested changes

--- src/themes/fonts.js
+++ src/themes/fonts.js
@@ -1,8 +1,8 @@
-const font_opensans = new FontFace("OpenSans", `url(https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap)`);
+const FontOpensans = new FontFace("OpenSans", `url(https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap)`);
 
 export const opensans = {
   type: {
-    primary: font_opensans,
+    primary: FontOpensans,
   },
   weight: {
     regular: "400",
@@ -49,6 +49,30 @@ export const rockwell = {
 export const tw_century_mt = {
   type: {
     primary: "tw_century_mt",
+  },
+  weight: {
+    regular: "400",
+    bold: "700",
+    extrabold: "800",
+    black: "900",
+  },
+  size: {
+    s1: 12,
+    s2: 14,
+    s3: 16,
+    m1: 20,
+    m2: 24,
+    m3: 28,
+    l1: 32,
+    l2: 40,
+    l3: 48,
+  },
+};
+
+const FontCalibreRegular = new FontFace("CalibreRegular", `../assets/fonts/Calibre-Regular.woff2`);
+export const CalibreRegular = {
+  type: {
+    primary: FontCalibreRegular,
   },
   weight: {
     regular: "400",
